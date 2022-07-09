0ba6868882dabe75432e56592e3d461244ffecbc
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2022-07-09 10:28:37 +0200

    few

--- Makefile
+++ Makefile
@@ -2,4 +2,5 @@ install-service:
 	install oda-bot.service $(HOME)/.config/systemd/user/oda-bot.service
 	systemctl --user daemon-reload
 	systemctl --user enable oda-bot.service 
+	install settings.toml -m 644 -Dv ~/.config/oda/bot
 	echo systemctl --user start oda-bot.service 
---
+++ odabot/cli.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+
+import logging
+import os
+import re
+import time
+import yaml
+import tempfile
+import click
+import subprocess
+from click.core import Context
+import requests
+import dynaconf
+
+logger = logging.getLogger()
+
+import logging_tree
+
+from dynaconf import Dynaconf
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load this files in the order.
+
+@click.group()
+@click.pass_obj
+def cli(obj):
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='\033[36m%(asctime)s %(levelname)s %(module)s\033[0m  %(message)s',
+    )
+    logger.info("default logging level INFO")
+
+    obj['settings'] = Dynaconf(
+        envvar_prefix="ODABOT",
+        settings_files=[
+            'settings.toml', 
+            '.secrets.toml',
+            os.path.join(os.getenv('HOME'), '.config/oda/bot/settings.toml')
+        ],
+    )
+
+
+    logger.info("components: %s", obj['settings'].components)
+
+
+@cli.command()
+@click.option('--branch', default="master")
+@click.argument('component')
+def update_chart(component, branch):    
+    with tempfile.TemporaryDirectory() as chart_dir:
+        subprocess.check_call([
+            "git", "clone", 
+            f"git@gitlab.astro.unige.ch:oda/{component}/{component}-chart.git",
+            chart_dir,
+            "--recurse-submodules",
+            "--depth", "1", #?
+        ])
+        # subprocess.check_call([
+        #     "git", "config", "commit.gpgsign", "false"
+        #     ])
+
+        try:
+            r = subprocess.check_call([
+                    "make", "-C", chart_dir, "update"
+                ],
+                env={**os.environ, 
+                     'GIT_CONFIG_COUNT': '1', 
+                     'GIT_CONFIG_KEY_0': 'commit.gpgsign',
+                     'GIT_CONFIG_VALUE_0': 'false'}
+            )
+            logger.error('\033[32msucceeded update (next to commit): %s\033[0m', r)
+            r = subprocess.check_call([
+                "git", "push", "origin", branch
+            ])
+        except subprocess.CalledProcessError as e:
+            logger.error('\033[31mcan not update (maybe no updates available?): %s\033[0m', e)
+
+
+@cli.command()
+@click.option('--source', default="orgs/oda-hub")
+@click.option('--forget', is_flag=True, default=False)
+@click.pass_context
+@click.pass_obj # in context too
+def poll_github_events(obj, ctx, source, forget):
+    min_poll_interval_s = 5
+    poll_interval_s = 60
+        
+    logger.info('staring oda-bot')
+
+    try:
+        last_event_id = yaml.safe_load(open('oda-bot-runtime.yaml'))[source]['last_event_id']
+        logger.info('found saved last event record %s', last_event_id)
+    except:
+        logger.warning('no saved last event record')
+        last_event_id = 0
+
+    if forget:
+        logger.warning('discarding last event record')
+        last_event_id = 0
+
+    while True:
+        try:
+            r = requests.get(f'https://api.github.com/{source}/events')
+        except Exception as e:
+            logger.warning("problem with connection! sleeping %s s", min_poll_interval_s)
+            time.sleep(min_poll_interval_s)
+            continue
+        
+        if int(r.headers['X-RateLimit-Remaining']) > 0:
+            poll_interval_s = (int(r.headers['X-RateLimit-Reset']) - time.time()) / int(r.headers['X-RateLimit-Remaining'])
+            logger.info('max average poll interval %.2f s', poll_interval_s)
+            poll_interval_s = max(min_poll_interval_s, poll_interval_s)            
+        else:
+            poll_interval_s = (int(r.headers['X-RateLimit-Reset']) - time.time())
+            logger.warning('rate limit exceeded! %d reset in %d', int(r.headers['X-RateLimit-Remaining']), poll_interval_s)
+            time.sleep(poll_interval_s)
+            continue
+
+        
+        if r.status_code != 200:
+            logger.error(r.text)
+            time.sleep(10)
+            continue
+
+        events = r.json()
+
+
+        n_new_push_event_by_component = {n: 0 for n in obj['settings']['components']}
+        new_last_event_id = last_event_id
+
+        for event in events:
+            if int(event['id']) > last_event_id:
+                new_last_event_id = max(int(event['id']), new_last_event_id)
+                logger.info("new event %s: %s %s %s %s", event['id'], event['repo']['name'], event['type'], event['payload'].get('ref', None), event['created_at'])
+                if event['type'] == 'PushEvent' and event['payload']['ref'] in ['refs/heads/master', 'refs/heads/main']:
+                    for n, comp in obj['settings']['components'].items():
+                        #('dispatcher' in event['repo']['name'] or 'oda_api' in event['repo']['name'] or 'oda_api' in event['repo']['name']):
+                        if any([re.search(trig, event['repo']['name']) for trig in comp['triggers']]) and event['repo']['name'] != f'{n}-chart':
+                            n_new_push_event_by_component[n] += 1
+
+        for n, comp in obj['settings']['components'].items():
+            if n_new_push_event_by_component[n] > 0:
+                logger.info("\033[33mgot %s new push events for %s, will update\033[0m", n_new_push_event_by_component[n], n)
+                ctx.invoke(update_chart, component=n)
+
+        logger.debug('returned last event ID %s', last_event_id)
+
+        if new_last_event_id > last_event_id:
+            yaml.dump({source: dict(last_event_id=new_last_event_id)}, open('oda-bot-runtime.yaml', "w"))
+            last_event_id = new_last_event_id
+            logger.info('new last event ID %s', last_event_id)
+        
+        time.sleep(min_poll_interval_s)            
+
+if __name__ == "__main__":
+    cli(obj={})
